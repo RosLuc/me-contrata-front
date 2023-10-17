9d6ba30eea00dea441fafd30c990671ba0401007
rainerio19 <rai.inerio@hotmail.com>
2023-10-17 00:37:28 -0300

    feat: list of itens in home

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -1,16 +1,32 @@
+import { ProductsFetchResponse } from "@/types/products-response";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosPromise } from "axios";
+import { useFilter } from "./useFilter";
+import { mountQuery } from "@/utils/get-category-by-type";
+import { useDeferredValue } from "react";
 
-const fetcher = () => {
-  //Update endpoint after back-end is ready
-  return axios.post('localhost:3333', `
-    
-  `)
-}
+const apiUrl = "http://localhost:3333/" as string;
 
-export function useProducts(){
-  const { } = useQuery({
-    queryFn: fetcher,
-    queryKey: ['products']
-  })
+const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
+	//Update endpoint after back-end is ready
+	return axios.post(apiUrl, {
+		query,
+	});
+};
+
+export function useProducts() {
+	const { type, priority, search } = useFilter();
+	const searchDeferred = useDeferredValue(search)
+	const query = mountQuery(type, priority);
+	const { data } = useQuery({
+		queryFn: () => fetcher(query),
+		queryKey: ["products", type, priority],
+	});
+
+	const products = data?.data.data?.allProducts
+	const filteredProducts = products?.filter(product => product.name.toLocaleLowerCase().includes(searchDeferred.toLocaleLowerCase()))
+
+	return {
+		data: filteredProducts
+	};
 }
