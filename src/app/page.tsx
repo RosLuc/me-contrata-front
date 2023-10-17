82b884a4e742e94ec4f8ee7f60625087e04b9090
rainerio19 <rai.inerio@hotmail.com>
2023-10-17 00:35:16 -0300

    feat: add icons and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-
+"use client"
 import { FilterBar } from '@/components/filter-bar'
 import styles from './page.module.css'
+import { ProductsList } from '@/components/products-list'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() {
+  const client = new QueryClient();
   return (
+    <QueryClientProvider client={client}>
    <main className={styles.main}>
       <FilterBar></FilterBar>
+      <ProductsList></ProductsList>
    </main>
+   </QueryClientProvider>
   )
 }
