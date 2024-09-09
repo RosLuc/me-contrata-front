a7b446fa786c36e89f8f30aacacc5b74a6a292b3
RosLuc <rosendolukas09@gmail.com>
2024-09-09 00:06:08 -0300

    refactor: changing to switch

--- src/utils/get-category-by-type.ts
+++ src/utils/get-category-by-type.ts
@@ -2,23 +2,36 @@ import { FilterType } from "@/types/filter-types";
 import { PriorityTypes } from "@/types/priority-types";
 
 export function getCategoryByType(type: FilterType) {
-  if (type == FilterType.PROGRAMMING) return 'PROGRAMMING';
-  if (type == FilterType.DESIGN) return 'DESIGN';
-  return "";
+
+  switch (type) {
+    case FilterType.PROGRAMMING:
+      return 'PROGRAMMING';
+    case FilterType.DESIGN:
+      return 'DESIGN';
+    default:
+      return "";
+  }
 }
 
 export function getFieldByPriority(priority: PriorityTypes) {
-  if (priority === PriorityTypes.NEWS)
-    return { field: "created_at", order: "ASC" };
-  if (priority === PriorityTypes.BIGGEST_PRICE)
-    return { field: "price", order: "DESC" };
-  if (priority === PriorityTypes.MINOR_PRICE)
-    return { field: "price", order: "ASC" };
-  return { field: "created_at", order: "ASC" };
+
+  switch (priority) {
+
+    case PriorityTypes.NEWS:
+      return { field: "created_at", order: "ASC" };
+    case PriorityTypes.BIGGEST_PRICE:
+      return { field: "price", order: "DESC" };
+    case PriorityTypes.MINOR_PRICE:
+      return { field: "price", order: "ASC" };
+    default:
+      return { field: "created_at", order: "ASC" };
+  }
 }
 
 export const mountQuery = (type: FilterType, priority: PriorityTypes) => {
+
   const category = getCategoryByType(type);
   const field = getFieldByPriority(priority);
+
   return `?category=${category}&orderBy=${field.field}&order=${field.order}`;
 };
